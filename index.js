911f856040b50adc7d5ea2e51b11e47bfbe87f51
gastón_Alonso() <tonga88@live.com.ar>
2024-04-28 21:33:46 -0300

    card mobile 375px

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const btnHamburger = document.getElementById('btnHamburger');
-const menuMobile = document.getElementById('menuMobile');
+// menu hamburguesa
+const btnHamburger = document.getElementById("btnHamburger");
+const menuMobile = document.getElementById("menuMobile");
+
+btnHamburger.addEventListener("click", () => {
+  menuMobile.classList.toggle("showMenu");
+});
+
+// slider de card
+const containerCard = document.getElementById("containerSld");
+const slides = document.querySelector(".slides");
+const bars = [...document.querySelectorAll(".bar")];
 
 
 
-
-btnHamburger.addEventListener('click', () => {
-    menuMobile.classList.toggle('showMenu');
+bars.forEach((i) => {
+  i.addEventListener("click", (e) => {
+    bars.forEach((bar) => bar.classList.remove("barActive"));
+    i.classList.add("barActive");
+   let index = bars.indexOf(i)
+    slides.style.transform = `translateX(-${index * slides.offsetWidth}px)`;
+    
+  });
 });
